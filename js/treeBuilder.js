7efd0419f3e7f9f0aec71b4086e93d5a276d8d50
Dawid <dawid.marchewczyk@gmail.com>
2023-01-06 16:14:06 +0100

    first steps

---
+++ js/treeBuilder.js
@@ -0,0 +1,16 @@
+function treeBuilder(n) {
+	if (Number.isInteger(n) && n > 0) {
+		const tree = [];
+		let stars = 1;
+		for (let i = 1; i <= n; i++) {
+			const newTreeBranch = new Array(stars).fill('*').join('');
+			tree.push(newTreeBranch);
+			console.log({ stars });
+			stars = i >= 1 ? i - 1 + 2 : stars;
+		}
+		console.log(tree.join('\n'));
+	} else {
+		console.error(`${n} It has to be an integer`);
+		return;
+	}
+}
