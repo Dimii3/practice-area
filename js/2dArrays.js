2b811d3361356e3d073596a1fc16d8ce8d442f43
Dawid <dawid.marchewczyk@gmail.com>
2023-01-10 19:58:54 +0100

    corrected code

--- js/2dArrays.js
+++ js/2dArrays.js
@@ -1,19 +1,19 @@
 const treeBuilder = (n) => {
+	if (!Number.isInteger(n) || n <= 0) {
+		console.error(`${n} is not an integer and your number has to be higher than 0`);
+		return;
+	}
 	const coreArr = [];
 	const whiteSpace = ' ';
 	const star = '*';
 	let stars = 1;
-	if (!Number.isInteger(n) && n > 0) {
-		console.error(`${n} is not an integer`);
-		return;
-	} else {
-		for (let i = 1; i <= n; i++) {
-			const newTreeBranch = new Array(stars).fill(star).join('');
-			let space = whiteSpace.repeat(n - (i - 1));
-			coreArr.push(space + newTreeBranch);
-			stars = i >= 1 ? stars + 2 : stars;
-		}
-		console.log(coreArr.join('\n'));
-		return coreArr;
+	for (let i = 1; i <= n; i++) {
+		const newTreeBranch = new Array(stars).fill(star).join('');
+		let space = whiteSpace.repeat(n - (i - 1));
+		coreArr.push(space + newTreeBranch);
+		stars = i >= 1 ? stars + 2 : stars;
 	}
+	console.log(coreArr.join('\n'));
+	return coreArr;
 };
+
