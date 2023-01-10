7918f39400010601f18319bcf2a01c60a4f18854
Dawid <dawid.marchewczyk@gmail.com>
2023-01-10 18:16:53 +0100

    The code has been corrected

--- js/2dArrays.js
+++ js/2dArrays.js
@@ -1,20 +1,18 @@
 const treeBuilder = (n) => {
 	const coreArr = [];
+	const star = '*';
 	let stars = 1;
-	if (Number.isInteger(n) && n > 0) {
+	if (!Number.isInteger(n) && n > 0) {
+		console.error(`${n} is not an integer`);
+		return;
+	} else {
 		for (let i = 1; i <= n; i++) {
-			const newTreeBranch = new Array(stars).fill('*').join('');
-			console.log({ i });
-			console.log({ stars });
+			const newTreeBranch = new Array(stars).fill(star).join('');
 			let space = ' '.repeat(n - (i - 1));
 			coreArr.push(space + newTreeBranch);
 			stars = i >= 1 ? stars + 2 : stars;
 		}
 		console.log(coreArr.join('\n'));
 		return coreArr;
-	} else {
-		console.error(`${n} is not an integer`);
-		return;
 	}
 };
-
